3b51e94ab4782e1d61c0b223002a2ef8f93cd25e
Keith howling <khowling@gmail.com>
2025-10-05 23:06:39 +0100

    Add .gitignore and clean repository structure

--- venv/lib/python3.10/site-packages/agent_framework/microsoft/__init__.pyi
+++
@@ -1,5 +0,0 @@
-# Copyright (c) Microsoft. All rights reserved.
-
-from agent_framework_copilotstudio import CopilotStudioAgent, __version__, acquire_token
-
-__all__ = ["CopilotStudioAgent", "__version__", "acquire_token"]
